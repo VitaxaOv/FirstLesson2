9812f549459bec470aadac79318e819d6f79e2cd
VitaxaOv <vit2013aly@mail.ru>
2022-10-15 10:39:40 +0300

    Add first taks

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine(square);
+
+
+if(1==1)
+{
+    Console.WriteLine("");
+}
+else{
+    Console.WriteLine("else");
+}
+
+
